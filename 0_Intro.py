4dd6db5334220d017f55ba523a519d2433b2b34e
Alexandr Satskov <s.satskov@gmail.ru>
2021-06-17 10:36:47 +0300

    first lesson

---
+++ 0_Intro.py
@@ -0,0 +1,25 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Чтобы OS не забирала наш порт, после каждого нашего прерывания скрипта
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 5000))
+server_socket.listen()
+
+while True:
+    print('Жду соединения')
+    client_socket, address = server_socket.accept()
+    print('Соединение:', address)
+
+    while True:
+        print('Жду данные')
+        request = client_socket.recv(4096)
+
+        if request:
+            response = 'Hello world\n'.encode()
+            client_socket.send(response)
+        else:
+            break
+        client_socket.close()
+        print('Вне внутреннего цикла')
+
